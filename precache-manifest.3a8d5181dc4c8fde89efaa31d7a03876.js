516894a1ae8d015fdc854558a833f402509321e0
Narmadhu <oknarmadhu10496@gmail.com>
2020-03-04 21:22:01 +0530

    Updates

---
+++ precache-manifest.3a8d5181dc4c8fde89efaa31d7a03876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e42166f00fefdffdaee5efae545f13",
+    "url": "/Currency-Converter-API/index.html"
+  },
+  {
+    "revision": "65f260aab673865f237e",
+    "url": "/Currency-Converter-API/static/css/main.78f6ffa9.chunk.css"
+  },
+  {
+    "revision": "20c0a589ab69991c58d0",
+    "url": "/Currency-Converter-API/static/js/2.7001282d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Currency-Converter-API/static/js/2.7001282d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f260aab673865f237e",
+    "url": "/Currency-Converter-API/static/js/main.e47b83bc.chunk.js"
+  },
+  {
+    "revision": "c2cf21159d5d5436e3f6",
+    "url": "/Currency-Converter-API/static/js/runtime-main.662e16be.js"
+  }
+]);
